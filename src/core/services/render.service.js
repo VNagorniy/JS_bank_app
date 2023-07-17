0e48ff8105d869d361ae4136451fa329d5a1166c
VNagorniy <vnagorniyrnd@gmail.com>
2023-07-17 21:01:47 +0300

    разработка render.service.js

---
+++ src/core/services/render.service.js
@@ -0,0 +1,47 @@
+class RenderService {
+  /**
+   * @param {string} html
+   * @param {Array} components
+   * @param {Object} [styles]
+   * @returns {HTMLElement}
+   */
+  htmlToElement(html, components = [], styles) {
+    const template = document.createElement('template');
+    template.innerHTML = html.trim();
+    const element = template.content.firstChild;
+
+    console.log(element);
+
+    return element;
+  }
+
+  /**
+   * @param {HTMLElement} parentElement
+   * @param {Array} components
+   */
+  #replaceComponentTags(parentElement, components) {
+    const componentTagPattern = /^component-/;
+    const allElements = parentElement.getElementsByTagName('*');
+
+    for (const element of allElements) {
+      const elementTagName = element.tagName.toLowerCase();
+      if (componentTagPattern.test(elementTagName)) {
+        const componentName = elementTagName.replace(componentTagPattern, '').replace(/-/g);
+
+        const foundComponent = components.find((Component) => {
+          const instance = Component instanceof ChildComponent ? Component : new Component();
+
+          return instance.constructor.name.toLowerCase() === componentName;
+        });
+
+        if (foundComponent) {
+          const componentContent = foundComponent instanceof ChildComponent ? foundComponent.render() : new foundComponent().render();
+          element.replaceWith(componentContent);
+        } else {
+          console.error(`Component "${componentName}" not found in the provided components array`);
+        }
+      }
+    }
+  }
+}
+export default new RenderService();
