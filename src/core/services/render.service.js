de2687e48cc82f06cd5537b154497536455f27f0
VNagorniy <vnagorniyrnd@gmail.com>
2023-07-24 23:47:33 +0300

    разработка модуля стилей в render.service.js + создание шаблона folder templates

--- src/core/services/render.service.js
+++ src/core/services/render.service.js
@@ -10,7 +10,11 @@ class RenderService {
     template.innerHTML = html.trim();
     const element = template.content.firstChild;
 
-    console.log(element);
+    if (styles) {
+      this.#applyModuleStyles(styles, element);
+    }
+
+    this.#replaceComponentTags(element, components);
 
     return element;
   }
@@ -42,6 +46,30 @@ class RenderService {
         }
       }
     }
+  }
+  /**
+   * @param {Object} moduleStyles
+   * @param {string} element
+   * @returns {void}
+   */
+  #applyModuleStyles(moduleStyles, element) {
+    if (!element) return;
+
+    const applyStyles = (element) => {
+      for (const [key, value] of Object.entries(moduleStyles)) {
+        if (element.classList.contains(key)) {
+          element.classList.remove(key);
+          element.classList.add(value);
+        }
+      }
+    };
+
+    if (element.getAttribute('class')) {
+      applyStyles(element);
+    }
+
+    const elements = element.querySelectorAll('*');
+    elements.forEach(applyStyles);
   }
 }
 export default new RenderService();
