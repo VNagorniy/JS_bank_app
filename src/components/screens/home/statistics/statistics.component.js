f9f1107b694adbd1e0477476061e6be00553e95d
VNagorniy <vnagorniyrnd@gmail.com>
2023-08-13 18:32:04 +0300

    Реализация круга транзакций

--- src/components/screens/home/statistics/statistics.component.js
+++ src/components/screens/home/statistics/statistics.component.js
@@ -11,6 +11,7 @@ import { LOADER_SELECTOR, Loader } from '@/components/ui/loader/loader.component
 import { $R } from '@/core/rquery/rquery.lib';
 import { StatisticsItem } from './statistics-item/statistics-item.component';
 import { formatToCurrency } from '@/utils/format/format-to-currency';
+import { CircleChart } from './circle-chart/circle-chart.component';
 
 export class Statistics extends ChildComponent {
   constructor() {
@@ -39,6 +40,24 @@ export class Statistics extends ChildComponent {
     this.#removeListeners();
   }
 
+  renderChart(income, expense) {
+    const total = income + expense;
+    let incomePercent = (income * 100) / total,
+      expensePercent = 100 - incomePercent;
+
+    if (income && !expense) {
+      incomePercent = 100;
+      expensePercent = 0.1;
+    }
+
+    if (!income && expense) {
+      incomePercent = 0.1;
+      expensePercent = 100;
+    }
+
+    return new CircleChart(incomePercent, expensePercent).render();
+  }
+
   fetchData() {
     this.statisticService.main((data) => {
       if (!data) return;
@@ -49,9 +68,14 @@ export class Statistics extends ChildComponent {
       const statisticsItemsElement = $R(this.element).find('#statistics-items');
       statisticsItemsElement.text('');
 
+      const circleChartElement = $R(this.element).find('#circle-chart');
+      circleChartElement.text('');
+
       statisticsItemsElement
         .append(new StatisticsItem('Income:', formatToCurrency(data[0].value), 'green').render())
         .append(new StatisticsItem('Expense:', formatToCurrency(data[1].value), 'purple').render());
+
+      circleChartElement.append(this.renderChart(data[0].value, data[1].value));
     });
   }
 
